544991ee20ba7b50dd909b17f506bbcf0f08e705
peterroe <peterroe@163.com>
2022-09-26 00:24:04 +0800

    feat: test

--- test/indes.test.ts
+++ test/indes.test.ts
@@ -1,13 +1,21 @@
 import { describe, expect, it } from 'vitest'
+import MarkdownIt from 'markdown-it'
+import comment_plugin from '../src/index'
+
+const md = new MarkdownIt()
+
+md.use(comment_plugin)
 
 describe('test', () => {
   it('should workd', () => {
-    expect([1, 2, 3].map(it => it ** it)).toMatchInlineSnapshot(`
-      [
-        1,
-        4,
-        27,
-      ]
+    const result = md.render(`
+    
+Hello [World]-This is a comment-, Please comment on this article
+    
+`)
+    expect(result).toMatchInlineSnapshot(`
+      "<p>Hello <div class=\\"content\\">World</div><div class=\\"comment\\">This is a comment</div>, Please comment on this article</p>
+      "
     `)
   })
 })
